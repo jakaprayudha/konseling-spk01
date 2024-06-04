9146573ac988740cc1ef876933e19aba57380b4a
yourusername <jakaprayudha3@gmail.com>
2024-06-04 09:38:00 +0700

    fix create kriteria

--- addd.php
+++ addd.php
@@ -1,15 +1,13 @@
 <?php
-	include('configdb.php');
-	$kriteria = $_POST['kriteria']; 
-	$bobot = $_POST['bobot'];
-	$cost_benefit = $_POST['cost_benefit'];
-	
-	$result = $mysqli->query("INSERT INTO kriteria SET `kriteria` = '".$kriteria."', `bobot` = '".$bobot."', `cost_benefit` = '".$cost_benefit."' WHERE `id_kriteria` = ".$_GET['id'].";");
-	if(!$result){
-		echo $mysqli->connect_errno." - ".$mysqli->connect_error;
-		exit();
-	}
-	else{
-		header('Location: kriteria.php');
-	}
-?>
+include('configdb.php');
+$kriteria = $_POST['kriteria'];
+$bobot = $_POST['bobot'];
+$cost_benefit = $_POST['cost_benefit'];
+
+$result = $mysqli->query("INSERT INTO kriteria (kriteria, bobot, cost_benefit) VALUES('$kriteria','$bobot','$cost_benefit')");
+if (!$result) {
+	echo $mysqli->connect_errno . " - " . $mysqli->connect_error;
+	exit();
+} else {
+	header('Location: kriteria.php');
+}
